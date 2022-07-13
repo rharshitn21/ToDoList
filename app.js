bc4706b872395acdc3927abcf6462570bc5d5283
rharshitn21 <rharshitn21@gmail.com>
2022-07-13 21:15:55 +0530

    final commit

--- app.js
+++ app.js
@@ -140,7 +140,10 @@ app.post("/delete/:listName", (req, res)=>{
 });
 
 
-
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
